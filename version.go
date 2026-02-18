c2582e956b8e1a560c892d7cefbae5a8576f1d85
theboringhumane <me@iamharsh.dev>
2026-02-18 16:53:29 +0530

    refactor: simplify version string definition to a direct assignment.

--- version.go
+++ version.go
@@ -1,14 +1,3 @@
 package xem
 
-import "fmt"
-
-const (
-	// Major version number
-	VersionMajor = 0
-	// Minor version number
-	VersionMinor = 1
-	// Patch version number
-	VersionPatch = 1
-)
-
-var Version = fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
+var Version = "0.1.1"
